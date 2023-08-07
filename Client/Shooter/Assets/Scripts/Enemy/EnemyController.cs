1eec0578b0b400c6cc1dc3583bbd9c76361f65cb
Kirill <abqwerty390@gmail.com>
2023-08-07 21:19:33 +0300

    done homework

--- Client/Shooter/Assets/Scripts/Enemy/EnemyController.cs
+++ Client/Shooter/Assets/Scripts/Enemy/EnemyController.cs
@@ -5,19 +5,22 @@ using UnityEngine;
 [RequireComponent(typeof(EnemyCharacter))]
 public class EnemyController : MonoBehaviour
 {
+    private Vector3 _newPosition;
+    private Vector3 _oldPosition = Vector3.zero;
+
     internal void OnChange(List<DataChange> changes)
     {
-        Vector3 position = transform.position;
-
         foreach (var dataChange in changes)
         {
             switch (dataChange.Field)
             {
                 case "x":
-                    position.x = (float)dataChange.Value;
+                    _oldPosition.x = (float)dataChange.PreviousValue;
+                    _newPosition.x = (float)dataChange.Value;
                     break;
                 case "y":
-                    position.z = (float)dataChange.Value;
+                    _oldPosition.z = (float)dataChange.PreviousValue;
+                    _newPosition.z = (float)dataChange.Value;
                     break;
                 default:
                     Debug.Log("Не обрабатывается изменение поля " + dataChange.Field);
@@ -25,6 +28,14 @@ public class EnemyController : MonoBehaviour
             }
         }
 
-        transform.position = position;  
+        if(_oldPosition != _newPosition)
+        {
+            _newPosition = Vector3.Lerp(_oldPosition, _newPosition, 5f);
+        }
+    }
+
+    private void Update()
+    {
+        transform.position = Vector3.Lerp(transform.position, _newPosition, 10f * Time.deltaTime);
     }
 }
