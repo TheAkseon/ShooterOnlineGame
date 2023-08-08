0f3c00297aaba584eadab63ad33a858686097ebb
Kirill <abqwerty390@gmail.com>
2023-08-08 09:51:29 +0300

    add physical player movement

--- Client/Shooter/Assets/Scripts/Player/PlayerCharacter.cs
+++ Client/Shooter/Assets/Scripts/Player/PlayerCharacter.cs
@@ -1,21 +1,31 @@
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
 public class PlayerCharacter : MonoBehaviour
 {
     [SerializeField] private float _speed = 2f;
 
+    private Rigidbody _rigidbody;
     private float _inputHorizontal;
     private float _inputVertical;
 
-    private void Update()
+    private void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+
+    private void  FixedUpdate()
     {
         Move();
     }
 
     private void Move()
     {
-        Vector3 direction = new Vector3(_inputHorizontal, 0, _inputVertical).normalized;
-        transform.position += direction * Time.fixedDeltaTime * _speed;
+        /*Vector3 direction = new Vector3(_inputHorizontal, 0, _inputVertical).normalized;
+        transform.position += direction * Time.fixedDeltaTime * _speed;*/
+
+        Vector3 velocity = (transform.forward * _inputVertical + transform.right * _inputHorizontal).normalized * _speed;
+        _rigidbody.velocity = velocity;
     }
 
     public void SetInput(float inputHorizontal, float inputVertical)
